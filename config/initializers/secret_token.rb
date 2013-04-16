6fd355d1ba8e10b5c87c0eb700215c124d03e75b
kshift.lv <kshift.lv@gmail.com>
2013-04-16 16:04:58 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snews::Application.config.secret_token = '2617a3d2126c453c3707cebef67011819b50d0244c2e31c6ff4cd1611a5c7cf7ae137df6a66023c56b1100ccabf3186cce13b42560f1f94e3f44bb8a2630a727'
