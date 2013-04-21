3448c646891a8d9901f9219b3d4f4e16cb259ee2
kshift.lv <kshift.lv@gmail.com>
2013-04-22 02:01:25 +0800

    Add prev button

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ StartupNews-API
 ===============
 
 抓取 StartupNews 文章条目，并用通 Readability API 对文章内容进行美化。
+
+访问 -> [Fake Starup News](http://news.kshift.me)
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,30 +1,35 @@
 //= require jquery_ujs
 //= require spin.min
+$("ul.post-list > li:gt(4):not(.page-button:last)").css('display', 'none');
 $("a.post-title").click(function() {
   $('.post-content *').remove();
   var spinner = new Spinner({top: '50px'}).spin();
   $('.post-content').append(spinner.el);
-  var post_id = $(this).attr("id");
-  $.get("/posts/" + post_id, function(result) {
+  $.get("/posts/" + $(this).attr("id"), function(result) {
     $('.post-content').html(result);
   });
+  spinner.stop();
 });
-$("ul.post-list > li:gt(4):not(#next)").css('display', 'none');
-$("a.next").click(function() {
+
+$("li.page-button").click(function() {
   var show_id = parseInt($("ul.post-list > li:visible").attr("class"), 0) ;
-  if ($(this).text() == 'Next') {
+  if ($(this).children().text() === 'Next') {
     show_id += 1;
-    $("ul.post-list > li:visible:not(#next)").hide();
+    $("ul.post-list > li:visible:not(.page-button)").hide();
     $("li." + show_id).fadeIn('slow');
-    if (show_id == 4) {
-      $(this).text('Prev');
+    if (show_id === 1) {
+      $(".page-button:first").fadeIn('slow');
+    }else if (show_id === 4) {
+      $(this).fadeOut('slow');
     }
-  }else if ($(this).text() == 'Prev') {
+  }else if ($(this).children().text() === 'Prev') {
     show_id -= 1;
-    $("ul.post-list > li:visible:not(#next)").hide();
+    $("ul.post-list > li:visible:not(.page-button)").hide();
     $("li." + show_id).fadeIn('slow');
-    if (show_id === 0) {
-      $(this).text('Next');
+    if (show_id === 3) {
+      $(".page-button:last").fadeIn('slow');
+    }else if (show_id === 0) {
+      $(this).fadeOut('slow');
     }
   }
 });
