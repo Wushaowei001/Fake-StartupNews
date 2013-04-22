42fc75e28fd0e9d874a034f6a7c4ba4d5c9f9784
kshift.lv <kshift.lv@gmail.com>
2013-04-22 16:18:45 +0800

    Change App title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Startup News"
+    base_title = "Fake Startup News"
     if page_title.empty?
       base_title
     else
