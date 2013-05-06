c1952b73cdcb122e825d3e62b5a4ae11a9446abe
kshift.lv <kshift.lv@gmail.com>
2013-05-06 10:33:36 +0800

    Hide post-list without jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,12 +3,12 @@
 (function( window, undefined ) {
   var spinner = new Spinner().spin();
   $('.page-inner').append(spinner.el);
-  $("ul.post-list > li:not(.0):not(.page-button:last)").css('display', 'none');
+  $(".page-button:first").css('display', 'none');
 
   $(document).pjax("a[data=data-pjax]", "[data=data-pjax-container]");
 
   $("li.page-button").click(function() {
-    var show_id = parseInt($("ul.post-list > li:visible").attr("class"), 0) ;
+    var show_id = parseInt($("ul.post-list > li:visible").attr("class"), 0);
     if ($(this).children().text() === 'Next') {
       show_id += 1;
       if (show_id >= 5) { show_id = 5; }
@@ -24,7 +24,7 @@
       if (show_id <= 0) { show_id = 0; }
       $("ul.post-list > li:visible:not(.page-button)").hide();
       $("li." + show_id).fadeIn('slow');
-      if (show_id === 3) {
+      if (show_id === 4) {
         $(".page-button:last").fadeIn('slow');
       }else if (show_id === 0) {
         $(this).fadeOut('slow');
